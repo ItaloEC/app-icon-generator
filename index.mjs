c68b696a31b9438c6dee4efc13b8b7ab688018d2
Italo Alves de Lima <italo_alves2222@hotmail.com>
2024-02-23 20:01:50 -0300

    [CHORE]: initial commit

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import Jimp from "jimp";
+
+// Função para redimensionar uma imagem para vários tamanhos
+async function resizeImage(inputPath, outputPath, size) {
+  const image = await Jimp.read(inputPath);
+  await image.resize(size, size).writeAsync(outputPath);
+}
+
+async function execute() {
+  const inputImagePath = "./icon.png";
+
+  const sizes = [512, 192, 64, 32, 24, 16];
+
+  for (const size of sizes) {
+    const outputImagePath = `images/imagem_${size}x${size}.png`;
+    await resizeImage(inputImagePath, outputImagePath, size);
+    console.log(
+      `Imagem redimensionada para ${size}x${size}: ${outputImagePath}`
+    );
+  }
+}
+
+execute().catch((error) => console.error(error));
